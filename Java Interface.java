075e0bfa67453ac5a1ccab83d949ed5eb61e4f67
Tanmay gaira <tanmaygaira911@gmail.com>
2025-09-08 20:16:06 +0530

    Add files via upload

---
+++ Java Interface.java
@@ -0,0 +1,20 @@
+Java Interface
+
+An Interface in Java programming language is
+ defined as an abstract type used to specify the
+  behaviour of a class. An interface in Java is a 
+  blueprint of a behaviour. A Java interface contains
+   static
+ constants and abstract methods.
+
+
+ Key Properties of Interface:
+
+The interface in Java is a mechanism to achieve abstraction.
+By default, variables in an interface are public, static and final.
+It is used to achieve abstraction and multiple inheritance in Java.
+It supports loose coupling (classes depend on behavior, not implementation).
+In other words, interfaces primarily define methods that other classes must implement.
+An interface in Java defines a set of behaviours that a class can implement,
+ usually representing a CAN-DO relationship,
+ but not always in every scenario
