9a16f3e3e220fb46a0124d8ef1bd7e54c19cc9de
Tanmay gaira <tanmaygaira911@gmail.com>
2025-09-16 21:34:13 +0530

    Add files via upload

---
+++ Frametest.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+ class Myframe extends Frame
+ {
+
+     Myframe()
+    {
+     
+        setSize(200,500);
+        setVisible(true);
+    }}
+    class Frametest{
+        public static void main(String[] args) {
+            Myframe f =new Myframe();
+        }
+    }
