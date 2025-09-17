0d88cbd2b1a84b629f530deba0aacd10a22e76fd
Tanmay gaira <tanmaygaira911@gmail.com>
2025-09-17 18:43:31 +0530

    Add files via upload

---
+++ Currenttime.java
@@ -0,0 +1,9 @@
+//Display Current Time
+import java.time.LocalTime;
+public class Currenttime {
+    public static void main(String[] args) {
+        LocalTime id =LocalTime.now();
+        System.err.println(id);
+    }
+    
+}
---
+++ Currenttimedate.java
@@ -0,0 +1,10 @@
+//Display Current Date and Time
+import java.time.LocalDateTime;
+
+public class Currenttimedate {
+    public static void main(String [] arg)
+{
+    LocalDateTime id = LocalDateTime.now();
+    System.out.println(id);
+}    
+}
