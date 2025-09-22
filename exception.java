30273ae18349ab2d9629822128d1f6414e3bdcaf
Tanmay gaira <tanmaygaira911@gmail.com>
2025-09-22 20:39:06 +0530

    add exception

---
+++ exception.java
@@ -0,0 +1,25 @@
+
+class exception {
+    public static void main(String[] args)
+    {
+        int n = 10;
+        int m = 0;
+
+        try {
+          
+            // Code that may throw an exception
+            int ans = n / m;
+            System.out.println("Answer: " + ans);
+        }
+        catch (ArithmeticException e) {
+          
+            // Handling the exception
+            System.out.println(
+                "Error: Division by zero is not allowed!");
+        }
+        finally {
+            System.out.println(
+                "Program continues after handling the exception.");
+        }
+    }
+}
