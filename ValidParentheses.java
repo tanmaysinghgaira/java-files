a4a08b9ac85448be80bab29beadc8c4a3489eace
Tanmay gaira <tanmaygaira911@gmail.com>
2025-10-22 18:22:26 +0530

    Create ValidParentheses.java

---
+++ ValidParentheses.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') st.push(')');
+            else if (c == '{') st.push('}');
+            else if (c == '[') st.push(']');
+            else {
+                if (st.isEmpty() || st.pop() != c) return false;
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}")); // true
+        System.out.println(isValid("(]")); // false
+    }
+}
