57b2d87b2435dc8705a74230a77c2660d34f474c
Tanmay gaira <tanmaygaira911@gmail.com>
2025-10-31 18:44:36 +0530

    Create Misam.java

---
+++ Misam.java
@@ -0,0 +1,37 @@
+ class Misam {
+    
+    String  name;
+    double salary;
+     
+    Misam (String n , double s )
+    {
+        name  =n ;
+        salary =s;
+
+    }
+    void display(){
+        System.out.println("Name  :"+ name);
+        System.out.println("salary :"+salary);
+
+    }
+}
+class Manager extends Misam {
+    String department;
+    Manager (String n,double s,String d){
+        super (n,s );
+      //calling parent class constructor
+   department=d;
+
+    }
+    void display(){
+        super.display();
+        System.out.println("Department :" + department);
+    }
+}
+
+class JavaFundamentals {
+    public static void main(String[] args) {
+        Manager m1 = new Manager("Ali", 700000, "IT");
+        m1.display();
+    }
+}
