5ca6fac1e332197ce5d06e320150ee83fa741d64
Tanmay gaira <tanmaygaira911@gmail.com>
2025-11-12 20:04:23 +0530

    new he ye code run nahi ho raha
    ggg

---
+++ NEWfolder/Check.java
@@ -0,0 +1,47 @@
+package NEWfolder/Check.java;
+
+/*inheritance in java is a core object oriented programming concept that allow one class */
+//to acquire the fields and methods of another class it promotes code reusability ,method overriding
+//and clear hierachical structure 
+class Animal{
+    String name;
+    
+    Animal(String name){
+        this.name =name;
+    }
+    //method
+void eat(){
+    System.out.println(name +"is eating");
+}
+}
+//child class
+
+class Rat extends  Animal{
+    String breed;
+    //constructor
+    Rat(String name,String breed){
+        super(name); //call parent constructor
+        this.breed=breed;
+        
+    }
+    void bark(){
+        System.out.println(name +"is barking");
+    }
+
+}
+
+public class Check{
+ public static void main(String[] args) {
+    try {
+        Rat rat = new Rat("BUDDY","HAMESTER");
+        rat.eat(); //inherited method
+        rat.bark(); //child's own method
+    } catch (Exception e) {
+        System.out.println("Error "+ e.getMessage());
+    }
+
+ }
+}
+    
+
+
