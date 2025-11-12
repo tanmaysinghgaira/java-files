19761df4df9291ccfd684cd6dd8cf24d7445899b
Tanmay gaira <tanmaygaira911@gmail.com>
2025-11-12 19:48:00 +0530

    learnig again with logic

---
+++ inheritens.java
@@ -0,0 +1,15 @@
+/*inheritance in java is a core object oriented programming concept that allow one class */
+//to acquire the fields and methods of another class it promotes code reusability ,method overriding
+//and clear hierachical structure 
+class Animal"{
+    String name;
+    
+    Animal(String name){
+        this.name =name;
+    }
+}
+
+ 
+public class inheritens {
+    
+}
