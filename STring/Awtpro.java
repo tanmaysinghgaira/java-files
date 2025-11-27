9ff267ee843c42cb27686102748bf16ab91f6926
Tanmay gaira <tanmaygaira911@gmail.com>
2025-11-27 19:29:58 +0530

    Update Awtpro.java

--- STring/Awtpro.java
+++ STring/Awtpro.java
@@ -1,4 +1,15 @@
 public class Awtpro {
     // awt is java's Abstract Windowing Toolkit it provides a set of APIs to create GUI applications in Java.
     // AWT provides a collection of classes for creating and managing windows, buttons, text fields, labels, and other graphical components.
+//commonly used AWT components
+    // Frame: A top-level window with a title and a border.
+    // Button: A clickable button that can trigger actions when pressed.
+    // Label: A non-editable text component used to display information.
+    // TextField: An editable text component that allows users to input text.
+    // TextArea: A multi-line text component for displaying or editing larger amounts of text.
+    // Checkbox: A component that represents a binary choice (checked or unchecked).
+    // RadioButton: A component that allows users to select one option from a group of choices.
+    // List: A component that displays a list of items from which users can select one or more.
+    // MenuBar, Menu, MenuItem: Components for creating menus in a window.
 }
+
