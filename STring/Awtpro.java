58320c57028f5b62216d739b5206486600e0e728
Tanmay gaira <tanmaygaira911@gmail.com>
2025-11-27 19:28:15 +0530

    Create Awtpro.java

---
+++ STring/Awtpro.java
@@ -0,0 +1,4 @@
+public class Awtpro {
+    // awt is java's Abstract Windowing Toolkit it provides a set of APIs to create GUI applications in Java.
+    // AWT provides a collection of classes for creating and managing windows, buttons, text fields, labels, and other graphical components.
+}
