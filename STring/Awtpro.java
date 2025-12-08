352ae44ce13cbbf0c4b99718a5d9d0150db5b8f8
Tanmay gaira <tanmaygaira911@gmail.com>
2025-12-08 19:33:54 +0530

    Update Awtpro.java

--- STring/Awtpro.java
+++ STring/Awtpro.java
@@ -72,3 +72,4 @@ class Abstraction
 //The Dog and Cat classes extend the Animal class and provide       specific implementations for the makeSound() method.
 //This demonstrates abstraction by allowing us to work with the Animal type without needing to know the specific details of each animal's sound implementation.
 //End of Abstraction in java
+//End of Awt in java
